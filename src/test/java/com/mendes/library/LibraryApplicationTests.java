66c090a1d03e2d53e2ce605cc2086b4c1b25950c
Miriam Mendes <miriammendes094@gmail.com>
2022-09-11 18:46:55 -0400

    initial commit

---
+++ src/test/java/com/mendes/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendes.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
