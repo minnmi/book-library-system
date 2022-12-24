9fde07f6add778e078a2b94fd13c5c7afba633fc
Miriam Mendes <miriammendes094@gmail.com>
2022-12-24 17:06:25 -0400

    feat: envers config

---
+++ src/main/resources/db/migration/V3__schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE revinfo (
+    rev integer NOT NULL,
+    revtstmp bigint,
+    CONSTRAINT revinfo_pkey PRIMARY KEY (rev)
+);
+
+CREATE TABLE user_aud (
+    id bigint NOT NULL,
+    rev integer NOT NULL,
+    revtype smallint,
+    email varchar(255) DEFAULT NULL,
+    name varchar(255) DEFAULT NULL,
+    password varchar(255) DEFAULT NULL,
+    CONSTRAINT pk_user_aud PRIMARY KEY (id, rev),
+    CONSTRAINT fk_user_revinfo FOREIGN KEY (rev) REFERENCES revinfo (rev)
+);
