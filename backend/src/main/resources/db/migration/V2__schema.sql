0d957e9916129ce8e2b8fc6dfb6dd1d61b0eeee6
Miriam Mendes <miriammendes094@gmail.com>
2023-02-21 15:01:16 -0400

    fix: migrations corrected

---
+++ backend/src/main/resources/db/migration/V2__schema.sql
@@ -0,0 +1,93 @@
+CREATE TABLE revinfo (
+    rev integer NOT NULL AUTO_INCREMENT,
+    revtstmp bigint,
+    PRIMARY KEY (rev)
+);
+
+CREATE TABLE user_aud (
+    id bigint NOT NULL,
+    rev integer NOT NULL,
+    revtype smallint,
+    email varchar(255) DEFAULT NULL,
+    name varchar(255) DEFAULT NULL,
+    password varchar(255) DEFAULT NULL,
+    PRIMARY KEY (id, rev),
+    CONSTRAINT fk_user_revinfo FOREIGN KEY (rev) REFERENCES revinfo (rev)
+);
+
+CREATE TABLE book_aud (
+    id bigint NOT NULL,
+    rev integer NOT NULL,
+    revtype smallint,
+    name varchar(255) NOT NULL,
+    isbn varchar(255) NOT NULL,
+    publisher_id bigint NOT NULL,
+    literature_category_id bigint DEFAULT NULL,
+    created_at datetime NOT NULL DEFAULT current_timestamp(),
+    deleted_at datetime DEFAULT NULL,
+    quantity int(11) DEFAULT NULL,
+    PRIMARY KEY (id, rev),
+    CONSTRAINT fk_book_revinfo FOREIGN KEY (rev) REFERENCES revinfo (rev)
+);
+
+CREATE TABLE author_aud (
+      id bigint NOT NULL,
+      rev integer NOT NULL,
+      revtype smallint,
+      name varchar(255) NOT NULL,
+      created_at datetime NOT NULL DEFAULT current_timestamp(),
+      deleted_at datetime DEFAULT NULL,
+      PRIMARY KEY (id, rev),
+      CONSTRAINT fk_author_revinfo FOREIGN KEY (rev) REFERENCES revinfo (rev)
+);
+
+CREATE TABLE `booking_aud` (
+   `id` bigint NOT NULL,
+   `rev` integer NOT NULL,
+   `revtype` smallint,
+   `user_id` bigint NOT NULL,
+   `book_id`bigint NOT NULL,
+   `current_date` timestamp NOT NULL DEFAULT current_timestamp(),
+   `priority` int(11) NOT NULL,
+   PRIMARY KEY (id, rev),
+   CONSTRAINT fk_booking_revinfo FOREIGN KEY (rev) REFERENCES revinfo (rev)
+);
+
+
+CREATE TABLE `literature_category_aud` (
+   `id` bigint NOT NULL,
+   `rev` integer NOT NULL,
+   `revtype` smallint,
+   `category_name` varchar(255) NOT NULL,
+   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+   `deleted_at` datetime DEFAULT NULL,
+   PRIMARY KEY (id, rev),
+   CONSTRAINT fk_literature_category_revinfo FOREIGN KEY (rev) REFERENCES revinfo (rev)
+);
+
+
+CREATE TABLE `loaned_aud` (
+  `id` bigint NOT NULL,
+  `rev` integer NOT NULL,
+  `revtype` smallint,
+  `final_date` datetime DEFAULT NULL,
+  `initial_date` datetime DEFAULT NULL,
+  `book_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `returned_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `returned` int(11) NOT NULL,
+  PRIMARY KEY (id, rev),
+  CONSTRAINT fk_loaned_revinfo FOREIGN KEY (rev) REFERENCES revinfo (rev)
+);
+
+CREATE TABLE `publisher_aud`
+(
+    `id` bigint NOT NULL,
+    `rev` integer NOT NULL,
+    `revtype` smallint,
+    `name` varchar(255) NOT NULL,
+    `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+    `deleted_at` datetime DEFAULT NULL,
+    PRIMARY KEY (id, rev),
+    CONSTRAINT fk_publisher_revinfo FOREIGN KEY (rev) REFERENCES revinfo (rev)
+);
--- backend/src/main/resources/db/migration/V4__schema.sql
+++ backend/src/main/resources/db/migration/V4__schema.sql
@@ -1,9 +1,122 @@
-create table configuration (
-    id int primary key ,
-    maximum_number_books_user int(8), -- quantidade máxima de livros por usuário
-    maximum_booking_period int(8), -- tempo máximo que o usuário pode ficar com o livro
-    proportion_books_stock float, -- proporção de livros que devem permanecer na biblioteca
-    CHECK (id = 1)
+ALTER TABLE user ADD username varchar(255) DEFAULT NULL;
+ALTER TABLE user ADD enabled tinyint(4) DEFAULT NULL;
+ALTER TABLE user ADD CONSTRAINT email_UNIQUE UNIQUE(email);
+
+ALTER TABLE user_aud ADD username varchar(255) DEFAULT NULL;
+ALTER TABLE user_aud ADD enabled tinyint(4) DEFAULT NULL;
+
+CREATE TABLE `roles` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL UNIQUE,
+    PRIMARY KEY (`id`)
 );
 
-insert into configuration values (1, 10, 7, 0.3);
+CREATE TABLE `users_roles` (
+    `user_id` bigint NOT NULL,
+    `role_id` bigint NOT NULL,
+     PRIMARY KEY (`user_id`, `role_id`),
+    CONSTRAINT `fk_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+    CONSTRAINT `fk_users_roles_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+);
+
+CREATE TABLE `authority` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL unique,
+     PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `role_authority` (
+    `role_id` bigint NOT NULL,
+    `authority_id` bigint NOT NULL,
+    PRIMARY KEY (`role_id`, authority_id),
+    CONSTRAINT `fk_authority` FOREIGN KEY (`authority_id`) REFERENCES `authority` (`id`),
+    CONSTRAINT `fk_role_authority_user` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+);
+
+INSERT INTO `roles` (`name`) VALUES ('ADMIN');
+INSERT INTO `roles` (`name`) VALUES ('LIBRARIAN');
+INSERT INTO `roles` (`name`) VALUES ('USER');
+
+INSERT INTO `authority` (`name`) VALUES ('USER_VIEW');
+INSERT INTO `authority` (`name`) VALUES ('USER_INSERT');
+INSERT INTO `authority` (`name`) VALUES ('USER_UPDATE');
+INSERT INTO `authority` (`name`) VALUES ('USER_DELETE');
+INSERT INTO `authority` (`name`) VALUES ('AUTHOR_VIEW');
+INSERT INTO `authority` (`name`) VALUES ('AUTHOR_INSERT');
+INSERT INTO `authority` (`name`) VALUES ('AUTHOR_UPDATE');
+INSERT INTO `authority` (`name`) VALUES ('AUTHOR_DELETE');
+INSERT INTO `authority` (`name`) VALUES ('BOOK_VIEW');
+INSERT INTO `authority` (`name`) VALUES ('BOOK_INSERT');
+INSERT INTO `authority` (`name`) VALUES ('BOOK_UPDATE');
+INSERT INTO `authority` (`name`) VALUES ('BOOK_DELETE');
+INSERT INTO `authority` (`name`) VALUES ('BOOKING_VIEW');
+INSERT INTO `authority` (`name`) VALUES ('BOOKING_INSERT');
+INSERT INTO `authority` (`name`) VALUES ('BOOKING_UPDATE');
+INSERT INTO `authority` (`name`) VALUES ('BOOKING_DELETE');
+INSERT INTO `authority` (`name`) VALUES ('LITERATURE_CATEGORY_VIEW');
+INSERT INTO `authority` (`name`) VALUES ('LITERATURE_CATEGORY_INSERT');
+INSERT INTO `authority` (`name`) VALUES ('LITERATURE_CATEGORY_UPDATE');
+INSERT INTO `authority` (`name`) VALUES ('LITERATURE_CATEGORY_DELETE');
+INSERT INTO `authority` (`name`) VALUES ('LOANED_VIEW');
+INSERT INTO `authority` (`name`) VALUES ('LOANED_INSERT');
+INSERT INTO `authority` (`name`) VALUES ('LOANED_UPDATE');
+INSERT INTO `authority` (`name`) VALUES ('LOANED_DELETE');
+INSERT INTO `authority` (`name`) VALUES ('PUBLISHER_VIEW');
+INSERT INTO `authority` (`name`) VALUES ('PUBLISHER_INSERT');
+INSERT INTO `authority` (`name`) VALUES ('PUBLISHER_UPDATE');
+INSERT INTO `authority` (`name`) VALUES ('PUBLISHER_DELETE');
+
+INSERT INTO `user` (`id`, `email`, `name`, `password`, `username`, `enabled`) VALUES (2, 'admin@lib.com', 'Admin', '123456', 'admin', 1);
+INSERT INTO `user` (`id`, `email`, `name`, `password`, `username`, `enabled`) VALUES (3, 'lib_foo@lib.com', 'Alex', '123456', 'lib.alex', 1);
+INSERT INTO `user` (`id`, `email`, `name`, `password`, `username`, `enabled`) VALUES (4, 'user_lib@lib.com', 'John', '123456', 'lib.john', 1);
+
+INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (2, 1);
+INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (3, 2);
+INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (4, 3);
+
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 1);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 2);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 3);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 4);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 5);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 6);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 7);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 8);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 9);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 10);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 11);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 12);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 13);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 14);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 15);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 16);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 17);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 18);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 19);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 20);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 21);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 22);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 23);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (1, 24);
+
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 5);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 6);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 7);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 8);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 9);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 10);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 11);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 12);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 13);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 14);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 15);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 16);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 17);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 18);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 19);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 20);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 21);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 22);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 23);
+INSERT INTO `role_authority` (`role_id`, `authority_id`) VALUES (2, 24);
