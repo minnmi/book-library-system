c4487e33fd46fd3014eb80e5e452ef9a7d27aa52
mct96-pc <matheuscandido2009@gmail.com>
2023-03-04 15:34:12 -0400

    Loan related notification

---
+++ backend/src/main/java/com/mendes/library/task/LoanedTask.java
@@ -0,0 +1,19 @@
+package com.mendes.library.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class LoanedTask {
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.DAYS)
+    public void notifyReading() {
+        // do it
+    }
+
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.HOURS)
+    public void notifyLoanedTimeOut() {
+        // do it
+    }
+}
