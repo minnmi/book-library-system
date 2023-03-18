24d98ff3bb6034d06d9666b653972a17a7f5d4ee
Miriam Mendes <miriammendes094@gmail.com>
2023-03-18 16:26:52 -0400

    feat: book refactor

--- backend/src/main/java/com/mendes/library/controller/BookController.java
+++ backend/src/main/java/com/mendes/library/controller/BookController.java
@@ -2,7 +2,8 @@ package com.mendes.library.controller;
 
 import com.google.zxing.WriterException;
 import com.mendes.library.model.Book;
-import com.mendes.library.model.DTO.BookDTO.BookDTO;
+import com.mendes.library.model.DTO.BookDTO.BookRequest;
+import com.mendes.library.model.DTO.BookDTO.BookResponse;
 import com.mendes.library.service.BookService;
 import com.mendes.library.service.QRCodeService;
 import jakarta.servlet.http.HttpServletResponse;
@@ -40,10 +41,10 @@ public class BookController {
 
     @PreAuthorize("hasAnyAuthority('BOOK_VIEW', 'ADMIN')")
     @GetMapping("/find/all")
-    public Page<BookDTO> findAllBooks(@PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+    public Page<BookResponse> findAllBooks(@PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
         var page = bookService.findAllBooks(pageable);
         var content = page.getContent().stream()
-                .map(object -> bookService.convertEntityToDto(object))
+                .map(book -> bookService.convertEntityToDto(book))
                 .collect(Collectors.toList());
 
         return new PageImpl<>(content, page.getPageable(), page.getTotalElements());
@@ -51,29 +52,29 @@ public class BookController {
 
     @PreAuthorize("hasAnyAuthority('BOOK_VIEW', 'ADMIN')")
     @GetMapping("/find/{id}")
-    public BookDTO findById(@PathVariable Long id) {
-        Book object = bookService.findById(id);
-        return bookService.convertEntityToDto(object);
+    public BookResponse findById(@PathVariable Long id) {
+        Book book = bookService.findById(id);
+        return bookService.convertEntityToDto(book);
     }
 
     @PreAuthorize("hasAnyAuthority('BOOK_INSERT', 'ADMIN')")
     @PostMapping("/insert")
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDTO insertBook(@Valid @RequestBody BookDTO objectDTO) {
-        log.info(" inserting a new book: {} ", objectDTO.getName());
-        Book objectRequest = bookService.convertDtoToEntity(objectDTO);
-        Book object = bookService.insertBook(objectRequest);
-        return bookService.convertEntityToDto(object);
+    public BookResponse insertBook(@Valid @RequestBody BookRequest bookRequest) {
+        log.info(" inserting a new book: {} ", bookRequest.getName());
+        Book bookEntity = bookService.convertDtoToEntity(bookRequest);
+        var book = bookService.insertBook(bookEntity);
+        return bookService.convertEntityToDto(book);
     }
 
     @PreAuthorize("hasAnyAuthority('BOOK_UPDATE', 'ADMIN')")
     @PutMapping("/update/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public BookDTO updateBook(@Valid @RequestBody BookDTO objectDTO, @PathVariable Long id) {
+    public BookResponse updateBook(@Valid @RequestBody BookRequest bookRequest, @PathVariable Long id) {
         log.info(" updating book of id: {} ", id);
-        Book objectRequest = bookService.convertDtoToEntity(objectDTO);
-        Book object = bookService.updateBook(id, objectRequest);
-        return bookService.convertEntityToDto(object);
+        Book bookEntity = bookService.convertDtoToEntity(bookRequest);
+        var book = bookService.updateBook(id, bookEntity);
+        return bookService.convertEntityToDto(book);
     }
 
 
