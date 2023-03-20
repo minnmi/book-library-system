940f9072457b765cebe77eb3b12778b3262544a9
Miriam Mendes <miriammendes094@gmail.com>
2023-03-20 13:58:58 -0400

    feat: user dto implemented

---
+++ backend/src/main/java/com/mendes/library/model/DTO/UserDTO/UserDTO.java
@@ -0,0 +1,16 @@
+package com.mendes.library.model.DTO.UserDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String name;
+}
