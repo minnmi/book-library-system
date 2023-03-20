932a3e21f3304a2c111a28ef5c2113ffde4fc6cb
Miriam Mendes <miriammendes094@gmail.com>
2023-03-20 13:56:57 -0400

    feat: validation for loan

--- backend/src/main/java/com/mendes/library/model/DTO/LoanedDTO/LoanRequest.java
+++ backend/src/main/java/com/mendes/library/model/DTO/LoanedDTO/LoanRequest.java
@@ -1,12 +1,15 @@
 package com.mendes.library.model.DTO.LoanedDTO;
 
+import com.mendes.library.model.DTO.BookDTO.BookDTO;
 import com.mendes.library.model.DTO.BookDTO.BookResponse;
+import com.mendes.library.model.DTO.UserDTO.UserDTO;
 import com.mendes.library.model.DTO.UserDTO.UserResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -18,8 +21,9 @@ public class LoanRequest implements Serializable {
     private static final long serialVersionUID = 1L;
     private LocalDateTime initialDate;
     private LocalDateTime finalDate;
-    private UserResponse user;
-    private BookResponse book;
+    private UserDTO user;
+    @NotNull(message = "The book is required.")
+    private BookDTO book;
     private Integer returned;
     private LocalDateTime returnedDate;
 
