9142674aca38199a676ee05477dfdfcd400aae03
Miriam Mendes <miriammendes094@gmail.com>
2023-03-20 14:25:58 -0400

    feat: booking dto

--- backend/src/main/java/com/mendes/library/model/DTO/BookindDTO/BookingResponse.java
+++ backend/src/main/java/com/mendes/library/model/DTO/BookindDTO/BookingResponse.java
@@ -1,4 +1,23 @@
 package com.mendes.library.model.DTO.BookindDTO;
 
+import com.mendes.library.model.DTO.BookDTO.BookDTO;
+import com.mendes.library.model.DTO.UserDTO.UserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingResponse {
+
+    private Long id;
+    private UserDTO user;
+    private BookDTO book;
+    private LocalDateTime currentDate;
+    private Integer priority;
 }
