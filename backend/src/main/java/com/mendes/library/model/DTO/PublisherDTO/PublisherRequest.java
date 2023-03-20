3acc2c6bab75f57443821e39a75ebb9a9521f660
Miriam Mendes <miriammendes094@gmail.com>
2023-03-20 13:57:32 -0400

    feat: user validation

--- backend/src/main/java/com/mendes/library/model/DTO/PublisherDTO/PublisherRequest.java
+++ backend/src/main/java/com/mendes/library/model/DTO/PublisherDTO/PublisherRequest.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class PublisherRequest {
+    @NotNull(message = "The name is required.")
     private String name;
 }
