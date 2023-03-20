b35357667e1a29a2e6196fc89a121b690261ef66
Miriam Mendes <miriammendes094@gmail.com>
2023-03-20 13:55:09 -0400

    feat: validations for category name

--- backend/src/main/java/com/mendes/library/model/DTO/LiteratureCategoryDTO/LiteratureCategoryRequest.java
+++ backend/src/main/java/com/mendes/library/model/DTO/LiteratureCategoryDTO/LiteratureCategoryRequest.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class LiteratureCategoryRequest {
-
+    @NotNull(message = "The category name is required.")
     private String categoryName;
 
 }
