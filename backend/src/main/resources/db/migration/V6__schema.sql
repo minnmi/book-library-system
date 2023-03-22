e1167690e74861cb9d8c6477db4b31e4827dcf84
Miriam Mendes <miriammendes094@gmail.com>
2023-03-21 21:49:44 -0400

    feat: new migration

---
+++ backend/src/main/resources/db/migration/V6__schema.sql
@@ -0,0 +1,29 @@
+--------------------------Insert into Publisher-------------------------------------------------------------------------
+INSERT INTO publisher (id, name) VALUES (1, 'O''Reilly');
+INSERT INTO publisher (id, name) VALUES (2, 'Stuart');
+INSERT INTO publisher (id, name) VALUES (3, 'Era');
+
+--------------------------Insert into Literature Category---------------------------------------------------------------
+INSERT INTO literature_category (id, category_name) VALUES (1, 'Fantasia');
+INSERT INTO literature_category (id, category_name) VALUES (2, 'Romance');
+INSERT INTO literature_category (id, category_name) VALUES (3, 'Literatura Clássica');
+INSERT INTO literature_category (id, category_name) VALUES (4, 'Terror');
+INSERT INTO literature_category (id, category_name) VALUES (5, 'Ficção');
+INSERT INTO literature_category (id, category_name) VALUES (6, 'Comédia');
+
+--------------------------Insert into Author----------------------------------------------------------------------------
+INSERT INTO author (id, name) VALUES (1, 'J K Rowling');
+INSERT INTO author (id, name) VALUES (2, 'John Doe');
+INSERT INTO author (id, name) VALUES (3, 'Charles Dickens');
+
+--------------------------Insert into Book------------------------------------------------------------------------------
+INSERT INTO book (id, name, isbn, publisher_id, literature_category_id, quantity, book_cover) VALUES (1, "Harry Potter e a Pedra Filosofal", 24239434, 1, 1, 10, null);
+INSERT INTO book (id, name, isbn, publisher_id, literature_category_id, quantity, book_cover) VALUES (2, "Amor das estrelas", 22332554, 2, 2, 25, null);
+INSERT INTO book (id, name, isbn, publisher_id, literature_category_id, quantity, book_cover) VALUES (3, "Cinco Novelas", 45488484, 3, 3, 48, null);
+
+--------------------------Insert into Book-Authorship-------------------------------------------------------------------
+INSERT INTO book_authorship (book_id, author_id) VALUES (1, 1);
+INSERT INTO book_authorship (book_id, author_id) VALUES (2, 2);
+INSERT INTO book_authorship (book_id, author_id) VALUES (3, 3);
+
+
