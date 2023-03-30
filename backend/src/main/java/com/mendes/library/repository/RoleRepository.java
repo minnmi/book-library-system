f960c1991245dc83794ae1d2d424d18ef8f80a22
Miriam Mendes <miriammendes094@gmail.com>
2023-03-30 13:47:06 -0400

    fix:  name of function for add roles

--- backend/src/main/java/com/mendes/library/repository/RoleRepository.java
+++ backend/src/main/java/com/mendes/library/repository/RoleRepository.java
@@ -2,12 +2,14 @@ package com.mendes.library.repository;
 
 import com.mendes.library.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Role findByName(String name);
+    Role findByName(String roleName);
+
 
 
 }
